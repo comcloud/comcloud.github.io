b624b4d0b38cd961c6d5d7b825cb73797c0f4d29
comcloud <2230817302@qq.com>
2019-10-05 14:54:46 +0800

    add dir bar

--- .vuepress/public/js/main.js
+++
@@ -1,4 +0,0 @@
-function init(){
-    console.log("执行了main.js");
-}
-setTimeout("init()",500)
--- .vuepress/sidebar.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	'/start/': require("../start/sidebar.js"),
-	
-	'/Java/web/kaifa': require('../Java/web/kaifa/sidebar'),
-	'/Java/web/framework': require('../Java/web/framework/sidebar'),
-}
